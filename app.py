b8cef7ac807ac345237554f8f56b41fc6eafead0
Justin Plasmeier <jplasmeier@gmail.com>
2016-12-03 01:12:57 -0800

    tasks development

--- app.py
+++ app.py
@@ -1,22 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+
 import os
+import requests
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import Result
 
-@app.route('/')
-def hello():
-    return "Hello, world!"
-
-
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    errors = []
+    results = {}
+    if request.method == 'POST':
+        try:
+            data = request.form
+            print("Title: ", data['title'])
+            print("Description: ", data['desc'])
+            print("Due Date: ", data['date'])
+            print("Duration: ", data['duration'])
+            print("Risk Factor: ", data['risk'])
+            print("Location: ", data['location'])
+        except Exception as err:
+            errors.append("Error: {}".format(err))
+    return render_template('index.html', errors=errors, results=results)
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -16,4 +16,26 @@ class Result(db.Model):
         self.result_no_stop_words = result_no_stop_words
 
     def __repr__(self):
-        return '<id {}>'.format(self.id) 
+        return '<id {}>'.format(self.id)
+
+
+class Task(db.Model):
+    __tablename__ = 'tasks'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String())
+    description = db.Column(db.String())
+    due_date = db.Column(db.DateTime())
+    duration = db.Column(db.Integer)
+    risk = db.Column(db.Float())
+    str_location = db.Column(db.String())
+
+    def __init__(self, title, description, due_date, duration, risk, str_location):
+        self.title = title
+        self.description = description
+        self.due_date = due_date
+        self.duration = duration
+        self.risk = risk
+        self.str_location = str_location
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
