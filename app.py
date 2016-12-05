b5ec92cdc2084859e2f08cc75ae9c6a64dce7e27
Justin Plasmeier <jplasmeier@gmail.com>
2016-12-05 01:03:13 -0800

    create endpoint

--- app.py
+++ app.py
@@ -15,17 +15,28 @@ from models import Task
 
 def create_and_save_task(data):
     try:
+        print('data in: ', data)
         task = Task(data['title'], data['desc'], data['date'], data['duration'], data['risk'], data['location'])
+        print('task before add', task)
         db.session.add(task)
         db.session.commit()
-        return task
+        print("task after commit ", task)
+        print("tasl dicgt", task.__dict__)
+        task.__dict__.pop('_sa_instance_state')
+        return task.__dict__
     except Exception as err:
         errors.append("Unable to add item to database: {}".format(err))
 
 
 def get_all_tasks():
     try:
-        return Task.query.all()
+        tasks = Task.query.all()
+        tasks_dict = {}
+        for task in tasks:
+            task.__dict__.pop('_sa_instance_state', None)
+            task_dict = task.__dict__
+            tasks_dict[task_dict['id']] = task_dict
+        return tasks_dict
     except:
         errors.append("Unable to get any existing tasks")
 
@@ -37,7 +48,10 @@ def get_task_by_id(id):
     :return:
     """
     try:
-        return Task.query.filter_by(id=id).first()
+        task_obj = Task.query.filter_by(id=id).first()
+        task_dict = task_obj.__dict__
+        task_dict.pop('_sa_instance_state', None)
+        return task_dict
     except:
         errors.append("Unable to return task id: {}".format(id))
 
@@ -52,28 +66,22 @@ def index():
 
 @app.route('/task', methods=['POST'])
 def create_task():
-    data = request.form
+    data = request.json
     task = create_and_save_task(data)
-    return jsonify(task, cls=new_alchemy_encoder(), check_circular=False)
+    print("errs", errors)
+    return jsonify(results=task)
 
 
 @app.route('/task', methods=['GET'])
 def read_tasks():
-    tasks = get_all_tasks()
-    tasks_dict = {}
-    for task in tasks:
-        task.__dict__.pop('_sa_instance_state', None)
-        task_dict = task.__dict__
-        tasks_dict[task_dict['id']] = task_dict
+    tasks_dict = get_all_tasks()
     return jsonify(results=tasks_dict)
 
 
 @app.route('/task/<id>', methods=['GET'])
 def read_task(id):
-    print('Getting task ', id)
-    task = get_task_by_id(id)
-    task.__dict__.pop('_sa_instance_state', None)
-    return jsonify(results=task.__dict__)
+    task_dict = get_task_by_id(id)
+    return jsonify(results=task_dict)
 
 
 @app.route('/task/<id>', methods=['PUT'])
